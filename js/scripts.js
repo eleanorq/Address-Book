b574d28d1e637d90feba7751f68b5ce85bf26618
Eleanor Q <elquandt@gmail.com>
2022-04-24 16:21:18 -0700

    add function to display details of contact on page

--- js/scripts.js
+++ js/scripts.js
@@ -35,27 +35,41 @@ function Contact(firstName, lastName, phoneNumber) {
     this.lastName = lastName;
     this.phoneNumber = phoneNumber;
 }
-
 Contact.prototype.fullName = function() {
     return this.firstName + " " + this.lastName;
 };
 
-
-let testContact = new Contact("Ada", "Lovelace", "503-555-1111");
-
-testContact
-this.currentID
 // User Interface Logic ----------
-let AddressBook = new AddressBook();
+let addressBook = new AddressBook();
+    //display the details of contacts
+function displayContactDetails(addressBookToDisplay) {
+    let contactsList = $("ul#contacts");
+    let htmlForContactInfo = "";
+    Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+        const contact = addressBookToDisplay.findContact(key);
+        htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+        });
+        contactsList.html(htmlForContactInfo);
+}
 
+    // form submission
 $(document).ready(function() {
     $("form#new-contact").submit(function(event) {
         event.preventDefault();
-        const inputtedFirstName= $("input#new-first-name").val();
-        const inputtedLastName= $("input#new-last-name").val();
+        const inputtedFirstName = $("input#new-first-name").val();
+        const inputtedLastName = $("input#new-last-name").val();
         const inputtedPhoneNumber = $("input#new-phone-number").val();
         let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
-            AddressBook.addContact(newContact);
+            addressBook.addContact(newContact);
             console.log(addressBook.contacts);
     });
 });
+
+
+/*using object.keys() to loop through address book's contacts to display the list of contacts
+for (const key in contact) {
+    if (contact.hasOwnProperty(key)) {
+      console.log(contact[key]);
+    }
+  }/*
+//object.keys() method returns all keys  in array and then loops over them
